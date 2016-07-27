3f66e25eb363e7a2194ea2644d9b4f780c6df27c
Ryan "archaen" Pando <vretha@gmail.com>
2016-07-27 16:16:55 -0600

    Addedn pictures to one column of footer.  Added map to footer.

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -11,7 +11,7 @@ $(function() {
     fullProductList(productObj);
   });
 
-  //grab on click from a size and return
+  //grab on click from a size link and return only those items that equal that size category
   $('#sort-by-size li a').on('click', function(event){
     event.preventDefault();
     var key = parseInt($(this).attr('value'));
@@ -23,7 +23,7 @@ $(function() {
       createProductElement(sortedSize);
     });
   });
-
+  // grab on click from a price link and return only those items that are within the twenty dollar range
   $('#sort-by-price li a').on('click', function(event){
     event.preventDefault();
     var key = parseFloat($(this).attr('value'));
@@ -31,23 +31,29 @@ $(function() {
     productObj.then(function(products){
       var sortedPrice = products.filter(function(index){
         var noDollarSign = index.price.replace('$', '');
-        return (noDollarSign <= (key + 20) && noDollarSign > key);
+        if (key === 80) {
+          return (noDollarSign > key);
+        }
+        else {
+          return (noDollarSign <= (key + 20) && noDollarSign > key);
+        }
       });
       createProductElement(sortedPrice);
     });
   });
 });
 
+//get Ajax Object as a promise
 function ajaxCall(url){
   return Promise.resolve($.ajax(url));
 }
-
+//create a div element on page per object in array
 function createProductElement(productObjArr){
   productObjArr.forEach(function(value){
-    $('#product-display').append('<figure class="products float-left"><img src="assets/' + value.id + '.png" alt="foobar"><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info"> <strong>Price:</strong> ' + value.price + '</p></figure>');
+    $('#product-display').append('<figure class="products float-left"><img src="assets/' + value.id + '.png" alt="foobar"><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p></figure>');
   });
 }
-
+//clear product display div
 function clearOutputDiv(){
   $('#product-display').empty();
 }
