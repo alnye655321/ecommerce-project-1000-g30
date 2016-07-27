48d893291a28d1e7cff4e7ea6541df15f1d41b1b
alnye655321 <alnye655321@gmail.com>
2016-07-26 21:10:18 -0600

    added jquery copy shipping to billing with checkbox click

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -2,5 +2,15 @@ $(function() {
   'use strict';
   console.log('sanity check');
 
+  $( "#copy" ).click(function() { //copy shipping to billing
+    $('#firstNameBilling').val($('#firstNameShipping').val());
+    $('#lastNameBilling').val($('#lastNameShipping').val());
+    $('#companyBilling').val($('#companyShipping').val());
+    $('#address1Billing').val($('#address1Shipping').val());
+    $('#address2Billing').val($('#address2Shipping').val());
+    $('#stateBilling').val($('#stateShipping').val());
+    $('#zipBilling').val($('#zipShipping').val());
+  });
+
 
 });
