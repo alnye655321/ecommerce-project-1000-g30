6b3e75308d154a43347520eb2f55b23d3aa34d2a
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-07-28 13:10:30 -0600

    Finished carousel with buttons *added 3 random products below carousel *nav bar need static shopping cart

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,17 @@ $(function() {
   'use strict';
 
   //console.log('sanity check');
-  // $.ajax({
-  //     url: 'http://galvanize-student-apis.herokuapp.com/gcommerce/products/',
-  //     method: 'GET',
-  //   }).done(function(results) {
-  //     //console.log(results);
-  //     for (var i = 0; i < 3; i++) {
-  //       var productPick = results[Math.floor(Math.random() * results.length)]
-  //       $('#carousel').append('<p>' + productPick.description + '</p>')
-  //       //console.log(productPick.description);
-  //    }
-  //   });
-
+  $.ajax({
+    url:'http://galvanize-student-apis.herokuapp.com/gcommerce/products/',
+    method: 'GET',
+    }).done(function(results) {
+      console.log(results);
+      var arr = [];
+      for (var i = 0; i < 3; i++) {
+        arr.push(results[Math.floor(Math.random() * results.length)]);
+    }
+    createProductElement(arr);
+  });
   //<----Image Carousel Start---->//
   //Set carousel variables and transition time
   var $carousel = $('.carousel');
@@ -38,12 +37,38 @@ $(function() {
               slides().eq($i + 1).fadeIn($transition_time);
               slides().eq($i + 1).addClass('active');
         }, $transition_time +  $time_between_slides);
+
   //<----Image Carousel Finish---->//
 
   //<----Back and Next Click Listeners---->
-    $('#back').on('click', function (event) {
-    //console.log('Button Werks');
-
-
-  });
+  $('#next').on('click',function(){
+    clearInterval(slides)
+    var $i = $carousel.find($slide + '.active').index();
+    slides().eq($i).removeClass('active');
+    slides().eq($i).hide();
+       //Start looping through images
+      if (slides().length === $i + 1) $i = -1;
+        slides().eq($i + 1).fadeIn();
+        slides().eq($i + 1).addClass('active');
 });
+  $('#back').on('click',function(){
+    clearInterval(slides)
+    var $i = $carousel.find($slide + '.active').index();
+    slides().eq($i).removeClass('active');
+    slides().eq($i).hide();
+       //Start looping through images
+      if (slides().length === $i - 1) $i = +1;
+        slides().eq($i - 1).fadeIn();
+        slides().eq($i - 1).addClass('active');
+});
+});
+//Generte 3 random products for homepage//
+  function createProductElement(productObjArr){
+    productObjArr.forEach(function(value){
+    $('#product-display').append('<figure class="col-md-4 products float-left bg-info"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:</strong></div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p><div class="purchase bg-prime text-center" style=" display: none"><h3>Purchase</h3></div></figure>');
+ });
+    var $divs = $('#product-display > figure');
+    for(var i = 0; i < $divs.length; i += 4) {
+    $divs.slice(i, i+4).wrapAll("<div class='row'></div>");
+  };
+};
