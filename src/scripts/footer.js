80179f0ee592e67197fb4e563889500cafca3684
Ryan "archaen" Pando <vretha@gmail.com>
2016-07-27 20:52:06 -0600

    Random star ratings added to each product.

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -6,9 +6,9 @@ function initMap() {
   var myLatLng = {lat: 39.7320, lng: -104.9839};
 
   var map = new google.maps.Map(document.getElementById('googleMap'), {
-    zoom: 15,
+    zoom: 16,
     center: myLatLng,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+    mapTypeId:'hybrid'
   });
 
   var marker = new google.maps.Marker({
--- src/scripts/products.js
+++ src/scripts/products.js
@@ -50,7 +50,7 @@ function ajaxCall(url){
 //create a div element on page per object in array
 function createProductElement(productObjArr){
   productObjArr.forEach(function(value){
-    $('#product-display').append('<figure class="products float-left"><img src="assets/' + value.id + '.png" alt="foobar"><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p></figure>');
+    $('#product-display').append('<figure class="products float-left bg-info"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:</strong> ' + randomStar() + '</div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p></figure>');
   });
 }
 //clear product display div
@@ -63,4 +63,25 @@ function fullProductList(productObj){
   productObj.then(function(products){
     createProductElement(products);
   });
+}
+
+function randomStar(){
+  var starNumber = Math.floor((Math.random() * 10) + 1)/2;
+  var fullStarBar = [];
+  console.log(starNumber);
+
+  for (var i = 0; i < 5; i++) {
+    if (starNumber > 0.5) {
+       fullStarBar.push('<i class="fa fa-star" aria-hidden="true"></i>');
+    }
+    else if (starNumber === 0.5) {
+      fullStarBar.push('<i class="fa fa-star-half-o" aria-hidden="true"></i>');
+    }
+    else {
+      fullStarBar.push('<i class="fa fa-star-o" aria-hidden="true"></i>');
+    }
+    starNumber--;
+  }
+    return fullStarBar.toString().replace(/,/g, '');
+
 }
