dd40c98cfac42e4520f3c6eba591386cc7f24b9c
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-07-28 14:45:17 -0600

    Added shopping cart overlay *fixed carousel *added products to the overlay

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,8 +10,10 @@ $(function() {
       var arr = [];
       for (var i = 0; i < 3; i++) {
         arr.push(results[Math.floor(Math.random() * results.length)]);
+
     }
     createProductElement(arr);
+     createShoppingList(arr);
   });
   //<----Image Carousel Start---->//
   //Set carousel variables and transition time
@@ -61,14 +63,40 @@ $(function() {
         slides().eq($i - 1).fadeIn();
         slides().eq($i - 1).addClass('active');
 });
+$('#button').on('click',function(){
+  $('#shoppin').show();
+});
 });
 //Generte 3 random products for homepage//
   function createProductElement(productObjArr){
     productObjArr.forEach(function(value){
-    $('#product-display').append('<figure class="col-md-4 products float-left bg-info"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:</strong></div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p><div class="purchase bg-prime text-center" style=" display: none"><h3>Purchase</h3></div></figure>');
+    $('#product-display').append('<figure class="col-md-4 products float-left bg-info"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:' + randomStar() + '</strong></div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p><div class="purchase bg-prime text-center" style=" display: none"><h3>Purchase</h3></div></figure>');
  });
     var $divs = $('#product-display > figure');
     for(var i = 0; i < $divs.length; i += 4) {
     $divs.slice(i, i+4).wrapAll("<div class='row'></div>");
   };
 };
+function createShoppingList(productObjArr){
+  productObjArr.forEach(function(value){
+  $('#shoppingList').append('<li class="bg-info products-cart"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:</strong></div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p><div class="purchase bg-prime text-center" style=" display: none"><h3>Purchase</h3></div></li>');
+});
+}
+function randomStar() {
+  var starNumber = Math.floor((Math.random() * 10) + 1)/2;
+  var fullStarBar = [];
+
+  for (var i = 0; i < 5; i++) {
+    if (starNumber > 0.5) {
+       fullStarBar.push('<i class="fa fa-star" aria-hidden="true"></i>');
+    }
+    else if (starNumber === 0.5) {
+      fullStarBar.push('<i class="fa fa-star-half-o" aria-hidden="true"></i>');
+    }
+    else {
+      fullStarBar.push('<i class="fa fa-star-o" aria-hidden="true"></i>');
+    }
+    starNumber--;
+  }
+    return fullStarBar.toString().replace(/,/g, '');
+  }
