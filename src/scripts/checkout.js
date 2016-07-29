fc021c8a21eb590acdb77bcf6f6f19a3ba088a96
Ho0dLuM <Jordon.Hoshor@gmail.com>
2016-07-29 10:16:00 -0600

    fixed linting errors on main.js and checkout.js

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -3,7 +3,7 @@ $(function() {
   console.log('sanity check');
   Stripe.setPublishableKey('pk_test_IDkwCdtBuXpEeMz1Nq5JOHe1'); //Stripe.js card validator api
 
-  $( "#copy" ).click(function() { //copy shipping to billing
+  $('#copy').click(function() { //copy shipping to billing
     $('#firstNameBilling').val($('#firstNameShipping').val());
     $('#lastNameBilling').val($('#lastNameShipping').val());
     $('#companyBilling').val($('#companyShipping').val());
@@ -20,173 +20,182 @@ $(function() {
 //Start Form Validation
 //Start Credit Validation
 
-$('form').on('submit', function(event){
+$('form').on('submit', function(event) {
   if ($('#lastNameShipping').val() !== 'Trump') {
 
-  event.preventDefault();
-  console.log('hijacked');
-  var creditNumber = $('#creditNumber').val();
-  var creditExpiration = $('#creditExpiration').val();
-  var creditCvc = $('#creditCvc').val();
+    event.preventDefault();
+    console.log('hijacked');
+    var creditNumber = $('#creditNumber').val();
+    var creditExpiration = $('#creditExpiration').val();
+    var creditCvc = $('#creditCvc').val();
 
-  var errorsPresent = false; // if any error is triggered record, if not display success message at bottom
+    var errorsPresent = false; // if any error is triggered record, if not display success message at bottom
 
-  if (Stripe.card.validateCardNumber($('#creditNumber').val()) === false) {
-    errorsPresent = true;
-    $("#creditNumber").css("border-color", "red"); //error causes input field border to turn red
-    $("#warning").append('<p>Not a Valid Credit Card Number</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#creditNumber").css("border-color", "silver");}  //if no error set border color to normal
+    if (Stripe.card.validateCardNumber($('#creditNumber').val()) === false) {
+      errorsPresent = true;
+      $('#creditNumber').css('border-color', 'red'); //error causes input field border to turn red
+      $('#warning').append('<p>Not a Valid Credit Card Number</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#creditNumber').css('border-color', 'silver');}  //if no error set border color to normal
 
-  if (Stripe.card.validateExpiry($('#creditExpiration').val()) === false) {
-    errorsPresent = true;
-    $("#creditExpiration").css("border-color", "red");
-    $("#warning").append('<p>Not a Valid Credit Card Expiration</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#creditExpiration").css("border-color", "silver");}
+    if (Stripe.card.validateExpiry($('#creditExpiration').val()) === false) {
+      errorsPresent = true;
+      $('#creditExpiration').css('border-color', 'red');
+      $('#warning').append('<p>Not a Valid Credit Card Expiration</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else {$('#creditExpiration').css('border-color', 'silver');}
 
-  if (Stripe.card.validateCVC($('#creditCvc').val()) === false) {
-    errorsPresent = true;
-    $("#creditCvc").css("border-color", "red");
-    $("#warning").append('<p>Not a Valid Credit Card CVC</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#creditCvc").css("border-color", "silver");}
-//End Credit Validation
+    if (Stripe.card.validateCVC($('#creditCvc').val()) === false) {
+      errorsPresent = true;
+      $('#creditCvc').css('border-color', 'red');
+      $('#warning').append('<p>Not a Valid Credit Card CVC</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#creditCvc').css('border-color', 'silver');}
+    //End Credit Validation
 
-//Start Regular Form Shipping Validation
-  if (($('#firstNameShipping').val()).length < 1) {
-    $("#firstNameShipping").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a First Name for Shipping</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#firstNameShipping").css("border-color", "silver");}
+    //Start Regular Form Shipping Validation
+    if (($('#firstNameShipping').val()).length < 1) {
+      $('#firstNameShipping').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a First Name for Shipping</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#firstNameShipping').css('border-color', 'silver');
+    }
 
-  if (($('#lastNameShipping').val()).length < 1) {
-    $("#lastNameShipping").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a Last Name for Shipping</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#lastNameShipping").css("border-color", "silver");}
+    if (($('#lastNameShipping').val()).length < 1) {
+      $('#lastNameShipping').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a Last Name for Shipping</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#lastNameShipping').css('border-color', 'silver');
+    }
 
-  if (($('#address1Shipping').val()).length < 1) {
-    $("#address1Shipping").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include an Address Line 1 for Shipping</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#address1Shipping").css("border-color", "silver");}
+    if (($('#address1Shipping').val()).length < 1) {
+      $('#address1Shipping').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include an Address Line 1 for Shipping</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#address1Shipping').css('border-color', 'silver');
+    }
 
-  if (($('#cityShipping').val()).length < 1) {
-    $("#cityShipping").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a City for Shipping</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#cityShipping").css("border-color", "silver");}
+    if (($('#cityShipping').val()).length < 1) {
+      $('#cityShipping').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a City for Shipping</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#cityShipping').css('border-color', 'silver');
+    }
 
-  if (($('#stateShipping').val()) == "NULL") {
-    $("#stateShipping").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a State for Shipping</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#stateShipping").css("border-color", "silver");}
+    if (($('#stateShipping').val()) == 'NULL') {
+      $('#stateShipping').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a State for Shipping</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else {$('#stateShipping').css('border-color', 'silver');
+    }
 
-  if (($('#zipShipping').val()).length < 5) {
-    $("#zipShipping").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a Zip for Shipping</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#zipShipping").css("border-color", "silver");}
-//End Regular Form Shipping Validation
+    if (($('#zipShipping').val()).length < 5) {
+      $('#zipShipping').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a Zip for Shipping</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#zipShipping').css('border-color', 'silver');
+    }
+    //End Regular Form Shipping Validation
 
-//Start Regular Form Billing Validation
-  if (($('#firstNameBilling').val()).length < 1) {
-    $("#firstNameBilling").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a First Name for Billing</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#firstNameBilling").css("border-color", "silver");}
+    //Start Regular Form Billing Validation
+    if (($('#firstNameBilling').val()).length < 1) {
+      $('#firstNameBilling').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a First Name for Billing</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#firstNameBilling').css('border-color', 'silver');
+    }
 
-  if (($('#lastNameBilling').val()).length < 1) {
-    $("#lastNameBilling").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a Last Name for Billing</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#lastNameBilling").css("border-color", "silver");}
+    if (($('#lastNameBilling').val()).length < 1) {
+      $('#lastNameBilling').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a Last Name for Billing</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#lastNameBilling').css('border-color', 'silver');
+    }
 
-  if (($('#address1Billing').val()).length < 1) {
-    $("#address1Billing").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include an Address Line 1 for Billing</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#address1Billing").css("border-color", "silver");}
+    if (($('#address1Billing').val()).length < 1) {
+      $('#address1Billing').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include an Address Line 1 for Billing</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#address1Billing').css('border-color', 'silver');
+    }
 
-  if (($('#cityBilling').val()).length < 1) {
-    $("#cityBilling").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a City for Billing</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#cityBilling").css("border-color", "silver");}
+    if (($('#cityBilling').val()).length < 1) {
+      $('#cityBilling').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a City for Billing</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#cityBilling').css('border-color', 'silver');
+    }
 
-  if (($('#stateBilling').val()) == "NULL") {
-    $("#stateBilling").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a State for Billing</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#stateBilling").css("border-color", "silver");}
+    if (($('#stateBilling').val()) == 'NULL') {
+      $('#stateBilling').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a State for Billing</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#stateBilling').css('border-color', 'silver');
+    }
 
-  if (($('#zipBilling').val()).length < 5) {
-    $("#zipBilling").css("border-color", "red");
-    errorsPresent = true;
-    $("#warning").append('<p>Please Include a Zip for Billing</p>');
-    $("#warning").show().delay(5000).fadeOut();
-  } else {$("#zipBilling").css("border-color", "silver");}
-//End Regular Form Billing Validation
+    if (($('#zipBilling').val()).length < 5) {
+      $('#zipBilling').css('border-color', 'red');
+      errorsPresent = true;
+      $('#warning').append('<p>Please Include a Zip for Billing</p>');
+      $('#warning').show().delay(5000).fadeOut();
+    } else { $('#zipBilling').css('border-color', 'silver');
+    }
+    //End Regular Form Billing Validation
 
-  if (errorsPresent === false) { //show success message if no errors
-    $("#success").append('<p>Your Order has Been Submitted for Proccessing</p>');
-    $("#success").show().delay(5000).fadeOut();
+    if (errorsPresent === false) { //show success message if no errors
+      $('#success').append('<p>Your Order has Been Submitted for Proccessing</p>');
+      $('#success').show().delay(5000).fadeOut();
+    }
+
+    setTimeout(//wait 5 seconds for success/error message generation, then clear out messages for new submit
+    function() {
+      $('#warning p').remove(); // clear warning messages for re-submit
+      $('#success p').remove(); //clear success message for re-submit
+    }, 5000); // five second timeout setting
   }
+  else {// hidden trump treasure below - src="assets/hidden/fileX"  to 176
+    event.preventDefault();
 
-
-  setTimeout( //wait 5 seconds for success/error message generation, then clear out messages for new submit
-  function()
-  {
-    $("#warning p").remove(); // clear warning messages for re-submit
-    $("#success p").remove(); //clear success message for re-submit
-  }, 5000); // five second timeout setting
-}
-else { // hidden trump treasure below - src="assets/hidden/fileX"  to 176
-  event.preventDefault();
-
-  $("#trump").append('<div class="alert alert-success" id="success" style="margin-top:10px; font-size:2em;"><strong>Thank you Donald! All of our inventory has been added to your shopping cart, and your delivery is on the way!</strong></div>');
-  loopTimeout(1, 176, 500, function(i){
-    $("#trump").append('<img src="assets/hidden/file' + i + '.jpg" class="img-responsive trump">');
-});
-}//end hidden trump
+    $('#trump').append('<div class="alert alert-success" id="success" style="margin-top:10px; font-size:2em;"><strong>Thank you Donald! All of our inventory has been added to your shopping cart, and your delivery is on the way!</strong></div>');
+    loopTimeout(1, 176, 500, function(i) {
+      $('#trump').append('<img src="assets/hidden/file' + i + '.jpg" class="img-responsive trump">');
+    });
+  }//end hidden trump
 
 });
 //End Form Validation
-
 
 //Start Zipcode Autocomplete
 //Will take a zip code and autofill state and city data
 //Also a possibility from just address, but that might be a little wonky.....
 //zip code api source: https://smartystreets.com/docs/us-zipcode-api
-$("#zipShipping").focus(function() { //selecting zip, entering data, then leaving will trigger the zip ajax call. Do nothing focus, but fire ajax on blur
+$('#zipShipping').focus(function() { //selecting zip, entering data, then leaving will trigger the zip ajax call. Do nothing focus, but fire ajax on blur
     console.log('in');
-}).blur(function() { // blur means a deselect of zip input, fires ajax call to zip code api
+  }).blur(function() { // blur means a deselect of zip input, fires ajax call to zip code api
     console.log('out'); // Install Postman! on google chrome
     var id = $(this).val();
     var ajaxCall = new Promise(function(resolve) { //declare a promise, wait for variables to set before appending state/city form fields
       var state, state_abbreviation, city;
       var ajaxArr = [];
       $.ajax({ // build an object pass into promise function
-      method: 'GET',
-      dataType: 'json',
-      url: 'https://us-zipcode.api.smartystreets.com/lookup?auth-id=d2e53a05-7def-56b1-e9c4-bcd4315c4e43&auth-token=2UPToivZOHdNv903mqV8&zipcode=' + id
-      }).done(function (result){
+        method: 'GET',
+        dataType: 'json',
+        url: 'https://us-zipcode.api.smartystreets.com/lookup?auth-id=d2e53a05-7def-56b1-e9c4-bcd4315c4e43&auth-token=2UPToivZOHdNv903mqV8&zipcode=' + id
+      }).done(function (result) {
         //state = result[0]['city_states'][0]['state'];
-        state_abbreviation = result[0]['city_states'][0]['state_abbreviation'];
-        city = result[0]['city_states'][0].city;
+        state_abbreviation = result[0].city_states[0].state_abbreviation;
+        city = result[0].city_states[0].city;
         ajaxArr = [state_abbreviation, city];
         resolve(ajaxArr);
       });
@@ -201,21 +210,21 @@ $("#zipShipping").focus(function() { //selecting zip, entering data, then leavin
       console.log(reason); // Error!
     });
 
-});
+  });
 //End Zipcode Autocomplete
 
 //Start Loop Timeout Function
 //will run a loop then delay after each iteration for var interval milliseconds. All for Trump! Make America Great Again!
- function loopTimeout(i, max, interval, func) {
-    if (i >= max) {
-        return;
-    }
-    // Call the function
-    func(i);
-    i++;
-    // "loop"
-    setTimeout(function() {
+function loopTimeout(i, max, interval, func) {
+  if (i >= max) {
+    return;
+  }
+  // Call the function
+  func(i);
+  i++;
+  // "loop"
+  setTimeout(function() {
         loopTimeout(i, max, interval, func);
-    }, interval);
+      }, interval);
 }
 //End Loop Timeout Function
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,17 @@ $(function() {
   //console.log('sanity check');
   $.ajax({
     url:'http://galvanize-student-apis.herokuapp.com/gcommerce/products/',
-    method: 'GET',
-    }).done(function(results) {
+    method: 'GET'
+  }).done(function(results) {
       console.log(results);
       var arr = [];
       for (var i = 0; i < 3; i++) {
         arr.push(results[Math.floor(Math.random() * results.length)]);
 
-    }
-    createProductElement(arr);
-     createShoppingList(arr);
-  });
+      }
+      createProductElement(arr);
+      createShoppingList(arr);
+    });
   //<----Image Carousel Start---->//
   //Set carousel variables and transition time
   var $carousel = $('.carousel');
@@ -25,86 +25,86 @@ $(function() {
   function slides() {
     return $carousel.find($slide);
   }
-    slides().fadeOut();
-      //Set active classes
-      slides().first().addClass('active');
-      slides().first().fadeIn($transition_time);
-        //Set up auto scroll function
-        var $interval = setInterval(function() {
+  slides().fadeOut();
+  //Set active classes
+  slides().first().addClass('active');
+  slides().first().fadeIn($transition_time);
+  //Set up auto scroll function
+  var $interval = setInterval(function() {
           var $i = $carousel.find($slide + '.active').index();
           slides().eq($i).removeClass('active');
           slides().eq($i).fadeOut($transition_time);
-             //Start looping through images
-            if (slides().length === $i + 1) $i = -1;
-              slides().eq($i + 1).fadeIn($transition_time);
-              slides().eq($i + 1).addClass('active');
+          //Start looping through images
+          if (slides().length === $i + 1) $i = -1;
+          slides().eq($i + 1).fadeIn($transition_time);
+          slides().eq($i + 1).addClass('active');
         }, $transition_time +  $time_between_slides);
 
   //<----Image Carousel Finish---->//
   //<----Click High-Jack---->//
-  $('form').on('submit', function(event){
+  $('form').on('submit', function(event) {
     event.preventDefault();
-    $('#form-output').append('<h3>You have been added to our email list!</h3>')
+    $('#form-output').append('<h3>You have been added to our email list!</h3>');
     $('#form-output').show().delay(5000).fadeOut();
   });
 
-  $('#emailSignup').on('keyup', function(event){
+  $('#emailSignup').on('keyup', function(event) {
     var email = $('#emailSignup').val();
-    if (!emailToCheck(email)){
+    if (!emailToCheck(email)) {
       $('#emailSignup').effect('shake');
-       $('#emailSignup').addClass('errorClass');
+      $('#emailSignup').addClass('errorClass');
     }
   });
 
   //<----Back and Next Click Listeners---->
-  $('#next').on('click',function(){
-    clearInterval(slides)
+  $('#next').on('click',function() {
+    clearInterval(slides);
     var $i = $carousel.find($slide + '.active').index();
     slides().eq($i).removeClass('active');
     slides().eq($i).hide();
-       //Start looping through images
-      if (slides().length === $i + 1) $i = -1;
-        slides().eq($i + 1).fadeIn();
-        slides().eq($i + 1).addClass('active');
+    //Start looping through images
+    if (slides().length === $i + 1) $i = -1;
+    slides().eq($i + 1).fadeIn();
+    slides().eq($i + 1).addClass('active');
   });
 
-  $('#back').on('click',function(){
-    clearInterval(slides)
+  $('#back').on('click',function() {
+    clearInterval(slides);
     var $i = $carousel.find($slide + '.active').index();
     slides().eq($i).removeClass('active');
     slides().eq($i).hide();
-       //Start looping through images
-      if (slides().length === $i - 1) $i = +1;
-        slides().eq($i - 1).fadeIn();
-        slides().eq($i - 1).addClass('active');
+    //Start looping through images
+    if (slides().length === $i - 1) $i = +1;
+    slides().eq($i - 1).fadeIn();
+    slides().eq($i - 1).addClass('active');
   });
 
-  $('#button').on('click',function(){
+  $('#button').on('click',function() {
     $('#shoppin').show();
   });
 });
 //Generte 3 random products for homepage//
-  function createProductElement(productObjArr){
-    productObjArr.forEach(function(value){
+function createProductElement(productObjArr) {
+  productObjArr.forEach(function(value) {
     $('#product-display').append('<figure class="col-md-4 products float-left bg-info"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:' + randomStar() + '</strong></div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p><div class="purchase bg-prime text-center" style=" display: none"><h3>Purchase</h3></div></figure>');
- });
-    var $divs = $('#product-display > figure');
-    for(var i = 0; i < $divs.length; i += 4) {
-    $divs.slice(i, i+4).wrapAll("<div class='row'></div>");
-  };
-};
-function createShoppingList(productObjArr){
-  productObjArr.forEach(function(value){
-  $('#shoppingList').append('<li class="bg-info products-cart"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:</strong></div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p><div class="purchase bg-prime text-center" style=" display: none"><h3>Purchase</h3></div></li>');
-});
+  });
+  var $divs = $('#product-display > figure');
+  for (var i = 0; i < $divs.length; i += 4) {
+    $divs.slice(i, i + 4).wrapAll('<div class="row"></div>');
+  }
+}
+function createShoppingList(productObjArr) {
+  productObjArr.forEach(function(value) {
+    $('#shoppingList').append('<li class="bg-info products-cart"><img src="assets/' + value.id + '.png" alt="foobar"><div><strong>Rating:</strong></div><p class=""> <strong>Description:</strong> ' + value.description + '</p><p class="text-info child"> <strong>Price:</strong> ' + value.price + '</p><div class="purchase bg-prime text-center" style=" display: none"><h3>Purchase</h3></div></li>');
+  });
 }
 function randomStar() {
-  var starNumber = Math.floor((Math.random() * 10) + 1)/2;
+  var starNumber = Math.floor((Math.random() * 10) + 1) / 2;
   var fullStarBar = [];
 
   for (var i = 0; i < 5; i++) {
     if (starNumber > 0.5) {
-       fullStarBar.push('<i class="fa fa-star" aria-hidden="true"></i>');
+      fullStarBar.push('<i class="fa fa-star" aria-hidden="true"></i>');
     }
     else if (starNumber === 0.5) {
       fullStarBar.push('<i class="fa fa-star-half-o" aria-hidden="true"></i>');
@@ -114,13 +114,13 @@ function randomStar() {
     }
     starNumber--;
   }
-    return fullStarBar.toString().replace(/,/g, '');
+  return fullStarBar.toString().replace(/,/g, '');
 }
 //email validater
 function emailVal(emailToCheck) {
   emailNoWS = emailToCheck.trim();
   var bool = true;
-  if (emailNoWS[0] === '@' ||  emailNoWS.indexOf('@') + 1 === emailNoWS.indexOf('.') || emailNoWS.indexOf('@') === -1 ){
+  if (emailNoWS[0] === '@' ||  emailNoWS.indexOf('@') + 1 === emailNoWS.indexOf('.') || emailNoWS.indexOf('@') === -1) {
     bool = false;
   }
   return bool;
